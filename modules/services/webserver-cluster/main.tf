ac9b41a25a706319a4d1ca1b974be1412da19f88
Mayeu <m@mayeu.me>
2019-07-12 19:04:56 +0200

    fix: load balancer deployment

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -53,29 +53,35 @@ resource "aws_launch_configuration" "example" {
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
-
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
+  target_group_arns    = [aws_lb_target_group.asg.arn]
+  health_check_type    = "ELB"
 
   min_size = var.min_size
   max_size = var.max_size
 
   tag {
     key                 = "Name"
-    value               = "terraform-asg-example"
+    value               = var.cluster_name
     propagate_at_launch = true
   }
 }
 
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  name = "${var.cluster_name}-instance"
+}
 
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "allow_server_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.instance.id
+
+  from_port   = var.server_port
+  to_port     = var.server_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group" "alb" {
+  name = "${var.cluster_name}-alb"
 }
 
 resource "aws_security_group_rule" "allow_http_inbound" {
@@ -99,25 +105,28 @@ resource "aws_security_group_rule" "allow_all_outbound" {
 
 }
 
-resource "aws_security_group" "alb" {
-  name = "${var.cluster_name}-alb"
-}
-
 resource "aws_lb" "example" {
-  name               = "asg-${var.cluster_name}"
+  name               = var.cluster_name
   load_balancer_type = "application"
   subnets            = data.aws_subnet_ids.default.ids
   security_groups    = [aws_security_group.alb.id]
 }
 
 resource "aws_lb_target_group" "asg" {
-  name     = "terraform-target-group-example"
+  name     = var.cluster_name
   port     = var.server_port
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
+
   health_check {
-    path = "/"
-    port = var.server_port
+    path                = "/"
+    protocol            = "HTTP"
+    port                = var.server_port
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
 }
 
